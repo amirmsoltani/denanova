026cf20f64a059f39da8c3265bed3abcafdd5918
amirasahraee <amirali.sf77@gmail.com>
2022-08-07 04:11:16 +0430

    feat: add layout and menu crusel

---
+++ layout/Header.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { MenuIcon } from "@heroicons/react/outline";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+
+const menuItems = [
+  { href: '/', name: 'خانه' },
+  { href: '/about-us', name: 'درباره ما' },
+  { href: '/contact-us', name: 'تماس با ما' },
+]
+
+const Header = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
+
+  const menuHandelr = () => { setMenuOpen(!menuOpen) };
+
+  const pathName = router.pathname;
+
+  return (
+    <div>
+      <div className='flex flex-1 justify-between'>
+
+        <button className='w-8' onClick={menuHandelr}>
+          <MenuIcon />
+        </button>
+
+        <div className='text-center'>
+          <h1 className='text-2xl font-bold'>گروه</h1>
+          <h1 className='text-6xl font-bold'>دنا نوا</h1>
+        </div>
+        <div className='w-8' />
+      </div>
+
+
+      <ul className={`${menuOpen ? "max-h-56" : "max-h-0"} overflow-hidden transition-all delay-500 flex flex-col divide-y divide-gray-400`}
+        dir='rtl'
+        onClick={menuHandelr}
+      >
+        {
+          menuItems.map((item) => (
+            <li key={item.href} className="flex flex-col justify-center py-4">
+              <Link href={item.href}>
+                <a className={`${item.href === pathName ? "font-bold" : "text-stone-400"} text-xl`}>
+                  {item.name}
+                </a>
+              </Link>
+            </li>))
+        }
+
+      </ul>
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: [
   "./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",
+  "./layout/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
+    fontFamily: {
+      sans: ['iranSans', 'sans-serif'],
+      serif: ['iranSans', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
