317a2c78565afc24851fbffadd2607837951aaa4
amirasahraee <amirali.sf77@gmail.com>
2022-08-10 01:08:54 +0430

    feat: create companys

--- pages/admin/file.tsx
+++ pages/admin/file.tsx
@@ -5,9 +5,27 @@ const File: NextPage = () => {
   return (
     <AdminWrapper>
       <div className="columns-3">
-        <div className="w-full h-96">
-          <img className="w-52 h-52" src="../../public/crusel.jpg" alt="" />
-          sss
+        <div className="w-full p-2 bg-red-400 h-72 ">
+          <img className="w-full h-52 p-4 " src="/crusel.jpg" alt="" />
+          <span className="block text-center bg-green-500">name</span>
+          
+          <button className="">Delete</button>
+          
+          <div className="w-full">
+            <span>sss</span>
+            </div>
+        </div>
+        <div className="w-full bg-red-400 h-72 ">
+          <img className="w-52 h-52" src="/crusel.jpg" alt="" />
+          <div className="w-full">
+            <span>sss</span>
+            </div>
+        </div>
+        <div className="w-full bg-red-400 h-72 ">
+          <img className="w-52 h-52" src="/crusel.jpg" alt="" />
+          <div className="w-full">
+            <span>sss</span>
+            </div>
         </div>
       </div>
     </AdminWrapper>
