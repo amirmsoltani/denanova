55bab504ef389813f68e14418d88723940c5c86a
amirasahraee <amirali.sf77@gmail.com>
2022-08-10 17:41:54 +0430

    feat: admin edit files

---
+++ pages/admin/files.tsx
@@ -0,0 +1,149 @@
+import type { NextPage } from "next";
+import AdminWrapper from "../../layout/adminWrapper";
+import { TrashIcon } from "@heroicons/react/solid";
+
+const File: NextPage = () => {
+  return (
+    <AdminWrapper>
+      <div className=" w-full py-5 px-12 " dir="ltr">
+        <div className="w-full flex">
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">1</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">2</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">3</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">4</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex">
+          <div className="w-56 h-52 m-2 p-2   drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">5</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">6</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">7</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">8</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex">
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">9</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">10</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">11</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2    drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">12</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full flex">
+          <div className="w-56 h-52 m-2 p-2 drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">13</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+          <div className="w-56 h-52 m-2 p-2  drop-shadow  bg-gray-100 ">
+            <img className="w-full p-2 " src="/crusel.jpg" alt="" />
+            <div className="w-full mt-2 flex justify-between px-2">
+              <span className="text-center text-md">14</span>
+              <button className="W-8 h-7 ">
+                <TrashIcon className="w-6 text-red-600 inline" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AdminWrapper>
+  );
+};
+
+export default File;
