e9974799d00629e23e0258b15ec34f00c85022e8
Amir M. Soltani <amir.soltani@sinansoft.com>
2022-08-26 04:58:57 +0430

    feat: send companies to component with props

--- pages/admin/companies.tsx
+++ pages/admin/companies.tsx
@@ -1,13 +1,51 @@
-import type { NextPage } from "next";
+import type { InferGetServerSidePropsType, NextPage } from "next";
 import AdminWrapper from "../../layout/adminWrapper";
 import TableCompany from "../../components/tableCompany";
+import { withAuthSsr, prisma } from "../../lib";
 
-const Post:NextPage =() => {
-    return(
-        <AdminWrapper>
-            <TableCompany />
-        </AdminWrapper>
-    );
+export const getServerSideProps = withAuthSsr(async ({ query }) => {
+  const pageSize = Math.abs(+(query.pageSize || 10));
+  const page = Math.abs(+(query.page || 1));
+  const search = (query.search as string) || "";
+
+  const companies = await prisma.post.findMany({
+    where: { title: { contains: search }, type: "company" },
+    take: pageSize,
+    skip: pageSize * (page - 1),
+    select: {
+      id: true,
+      title: true,
+      createAt: true,
+      updateAt: true,
+      author: { select: { fullname: true } },
+    },
+  });
+
+  const counts = await prisma.file.count();
+  const lastPage = Math.ceil(counts / pageSize);
+
+  return {
+    props: {
+      content: companies.map((company) => ({
+        ...company,
+        createAt: company.createAt.toISOString(),
+        updateAt: company.updateAt.toISOString(),
+      })),
+      pagination: { pageSize, page, counts, lastPage },
+    },
+  };
+});
+
+type PropsType = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+const Post: NextPage<PropsType> = ({ content, pagination }) => {
+  console.log(content, pagination);
+
+  return (
+    <AdminWrapper>
+      <TableCompany />
+    </AdminWrapper>
+  );
 };
 
 export default Post;
