89fd86ff842db128e9ea4206483378501c38de5c
amirasahraee <amirali.sf77@gmail.com>
2022-08-28 05:45:00 +0430

    feat: modal addPost is done

--- pages/admin/addPost.tsx
+++ pages/admin/addPost.tsx
@@ -6,18 +6,47 @@ import Editor from "../../components/editor";
 import Modal from "../../components/modal";
 import { useState } from "react";
 
-const AddPost: NextPage = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [file, setFile] = useState<Array<{
-    name: string;
-    filePath: string;
-    createAt: string;
-    id: number;
-  }>>([]);
+type FileType = {
+  name: string;
+  filePath: string;
+  createAt: string;
+  id: number;
+};
 
-  const modalHandler = () => {
+const AddPost: NextPage = () => {
+  const [getFile, setGetFile] = useState<
+    Array<{
+      name: string;
+      filePath: string;
+      createAt: string;
+      id: number;
+    }>
+  >([]);
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [multiChange, setMultiChange] = useState(false);
+
+  const [file, setFile] = useState<
+    | undefined
+    | Array<{
+        name: string;
+        filePath: string;
+        createAt: string;
+        id: number;
+      }>
+  >([]);
+
+  const [fileCoverPost, setFileCoverPost] = useState<undefined | FileType>();
+
+  const [statusRelFile, setStatusRelFile] = useState(false);
+
+  const modalHandler = (status: string) => {
     setModalOpen(!modalOpen);
-    !modalOpen && apiHandler();
+    setMultiChange(!multiChange);
+    !multiChange && apiHandler();
+
+    if (status === "cover") setStatusRelFile(false);
+    if (status === "multi") setStatusRelFile(true);
   };
 
   const apiHandler = async () => {
@@ -25,13 +54,73 @@ const AddPost: NextPage = () => {
       method: "get",
     });
     const result = await response.json();
-
     setFile(result.contents);
   };
 
+  const addImageCoverPost = (item: FileType) => {
+    if (statusRelFile === false) {
+      setFileCoverPost(item);
+    }
+    if (statusRelFile === true) {
+      setGetFile((oldFile) => [...oldFile, item]);
+      setMultiChange(!multiChange);
+    }
+    setModalOpen(!modalOpen);
+  };
+
+  const propsImage = () => {
+    let property = [];
+    property.push(
+      getFile.map((item, index) => {
+        return (
+          <div className="w-32 h-36 mt-8 px-1">
+            <div className="w-full h-32">
+              <img className="w-32 h-32" src={item.filePath} alt="" />
+            </div>
+            <div className="w-32 mt-3  flex justify-center">
+              <XCircleIcon
+                onClick={() => removeFile(index)}
+                className="w-6  text-red-600 hover:text-red-400 hover:cursor-pointer"
+              />
+            </div>
+          </div>
+        );
+      })
+    );
+
+    return property;
+  };
+
+  const removeFile = (index: number) => {
+    getFile.splice(index, 1);
+    !multiChange && apiHandler();
+  };
+
+
+  const onSubmit = async (event:any) => {
+    event.preventDefault();
+    const form = new FormData()
+   form.append("type",event.target["type"].value);
+   form.append("title", event.target["title"].value);
+   form.append( "description", event.target["description"].value);
+   form.append( "content" , event.target["content"].value);
+
+    
+   const response = await fetch("/api/admin/post", {
+    method: "POST",
+    body: form,
+  });
+
+
+  }
+
   return (
     <>
-      <Modal width="w-11/12" visible={modalOpen} onClose={modalHandler}>
+      <Modal
+        width="w-11/12"
+        visible={modalOpen}
+        onClose={() => modalHandler("not")}
+      >
         <div className="w-full flex justify-center">
           <div className="w-96 bg-slate-200 rounded-full flex  py-1 px-1 border border-slate-700">
             <input
@@ -47,6 +136,7 @@ const AddPost: NextPage = () => {
             <div
               key={item.id}
               className="w-32 h-auto m-2 p-2 drop-shadow  bg-gray-100 "
+              onClick={() => addImageCoverPost(item)}
             >
               <img className="w-full p-2 " src={item.filePath} alt="" />
               <div className="w-full mt-2 flex justify-center px-2">
@@ -59,7 +149,7 @@ const AddPost: NextPage = () => {
 
       <AdminWrapper>
         <div className="p-10">
-          <form name="formPost" action="">
+          <form name="formPost" onSubmit={onSubmit}>
             <label htmlFor="typePost" className="block">
               نوع محتوا :
             </label>
@@ -103,16 +193,23 @@ const AddPost: NextPage = () => {
             <span className="block w-full mt-5">عکس اصلی :</span>
             <div className="w-full p-1 mt-5 flex justify-center">
               <div className="w-96 flex flex-col items-center">
-                <div className="w-96 h-96" onClick={modalHandler}>
-                  {false ? (
-                    <img className="w-full h-full" src="/crusel.jpg" alt="" />
+                <div
+                  className="w-96 h-96"
+                  onClick={() => modalHandler("cover")}
+                >
+                  {fileCoverPost ? (
+                    <img
+                      className="w-full h-full"
+                      src={fileCoverPost.filePath}
+                      alt=""
+                    />
                   ) : (
                     <div className="flex items-center justify-center h-96 w-96 border border-black bg-white hover:bg-lime-50 hover:cursor-pointer hover:text-lime-600 hover:border-lime-600">
                       <PlusIcon className=" w-12 " />
                     </div>
                   )}
                 </div>
-                {false && (
+                {fileCoverPost && (
                   <div className="w-32 mt-1">
                     <XCircleIcon className="w-6 mr-12 text-red-600 hover:text-red-400 hover:cursor-pointer" />
                   </div>
@@ -120,26 +217,23 @@ const AddPost: NextPage = () => {
               </div>
             </div>
             <span className="mt-5">عکس های اسلایدر :</span>
-            <div className="w-full p-1 h-36 mt-5 flex" dir="ltr">
-              <div className="w-32 h-36 ">
-                <div className="w-full h-32">
-                  <img className="w-32 h-32" src="/crusel.jpg" alt="" />
-                </div>
-                <div className="w-32 mt-3  flex justify-center">
-                  <XCircleIcon className="w-6  text-red-600 hover:text-red-400 hover:cursor-pointer" />
-                </div>
-              </div>
-
-              <div className="w-32 ml-2">
+            <div className="w-full h-auto">
+              <div
+                className="w-full flex flex-wrap justify-around items-center h-auto"
+                dir="ltr"
+              >
+                <>{!multiChange && propsImage()}</>
                 <div
-                  onClick={modalHandler}
-                  className=" w-full h-32 bg-white flex items-center justify-center border border-black text-black hover:text-blue-600 hover:bg-blue-50 cursor-pointer hover:border-blue-600"
+                  onClick={() => modalHandler("multi")}
+                  className="w-32 ml-2"
                 >
-                  <PlusIcon className=" w-8 " />
+                  <div className=" w-full h-32 mt-8 bg-white flex items-center justify-center border border-black text-black hover:text-blue-600 hover:bg-blue-50 cursor-pointer hover:border-blue-600">
+                    <PlusIcon className=" w-8 " />
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="w-full mt-5 flex justify-center">
+            <div className="w-full mt-14 flex justify-center">
               <input
                 type="submit"
                 value="ارسال"
