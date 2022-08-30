cf0c59a5afd5d1553453a5aecbde7c5616f1fe51
amirasahraee <amirali.sf77@gmail.com>
2022-08-30 17:08:58 +0430

    feat: products show is done,delete will be done.

--- pages/admin/products.tsx
+++ pages/admin/products.tsx
@@ -7,6 +7,8 @@ import {
   TrashIcon,
   EyeIcon,
 } from "@heroicons/react/24/solid";
+import Router from "next/router";
+import Link from "next/link";
 
 export const getServerSideProps = withAuthSsr(async ({ query }) => {
   const pageSize = Math.abs(+(query.pageSize || 10));
@@ -41,11 +43,15 @@ export const getServerSideProps = withAuthSsr(async ({ query }) => {
   };
 });
 
+
+ const deletePost = async(id:number) => {
+  console.log(id)
+ }
+
 type PropsType = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 const Post: NextPage<PropsType> = ({ content, pagination }) => {
-  console.log(content, pagination);
-
+  // console.log(content, pagination);
 
 
   return (
@@ -62,9 +68,13 @@ const Post: NextPage<PropsType> = ({ content, pagination }) => {
               <td className="border-l border-gray-500 text-sm">{item.createAt}</td>
               <td className="border-l border-gray-500 text-sm">{item.updateAt}</td>
               <td className="p-2 flex items-center justify-between">
-                <EyeIcon className="w-5 text-stone-700 inline" />
+                <Link href={"/product/"+item.id}>
+                  <EyeIcon href="" className="w-5 text-stone-700 inline" />
+                </Link>
+                <Link href={"/admin/addPost?id="+item.id}>
                 <PencilSquareIcon className="w-5 text-lime-600  inline" />
-                <TrashIcon className="w-5 text-red-600 inline" />
+                </Link>
+                <TrashIcon onClick={() => deletePost(item.id)} className="w-5 text-red-600 inline" />
               </td>
             </tr>
           );
