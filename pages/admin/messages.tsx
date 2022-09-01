6cbcbf40b7ebba97a0abcc9944a782b9a333350e
amirmsoltani <amirsoltanik78@gmail.com>
2022-09-01 22:49:38 +0430

    feat: admin contact us messages back end

--- pages/admin/messages.tsx
+++ pages/admin/messages.tsx
@@ -1,16 +1,57 @@
-import { NextPage } from "next";
+import { InferGetServerSidePropsType, NextPage } from "next";
 import { AdminWarpper } from "../../layout";
 import TableMessage from "../../components/tableMessage";
 import Modal from "../../components/modal";
 import { useState } from "react";
+import { prisma, withAuthSsr } from "../../lib";
 
-const Post: NextPage = () => {
+export const getServerSideProps = withAuthSsr(async ({ query }) => {
+  const pageSize = Math.abs(+(query.pageSize || 10));
+  const page = Math.abs(+(query.page || 1));
+  const search = (query.search as string) || "";
 
-  const [modalOpen , setModalOpen] = useState(false);
-  const modalHandler = ()=>{
+  const messages = await prisma.contantUs.findMany({
+    where: { subject: { contains: search } },
+    take: pageSize,
+    skip: pageSize * (page - 1),
+    select: {
+      id: true,
+      subject: true,
+      createAt: true,
+      fullName: true,
+      email: true,
+      Content: true,
+      read: true,
+    },
+  });
+
+  const counts = await prisma.contantUs.count({
+    where: { subject: { contains: search } },
+  });
+
+  const lastPage = Math.ceil(counts / pageSize);
+
+  return {
+    props: {
+      content: messages.map((message) => ({
+        ...message,
+        createAt: message.createAt.toISOString(),
+      })),
+      pagination: { pageSize, page, counts, lastPage },
+    },
+  };
+});
+
+type PropsType = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+const Post: NextPage<PropsType> = ({ content, pagination }) => {
+  
+  console.log(content, pagination);
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalHandler = () => {
     setModalOpen(!modalOpen);
   };
-
 
   return (
     <>
@@ -25,15 +66,20 @@ const Post: NextPage = () => {
             <p className="">1401/5/21</p>
           </div>
           <p className="text-justify mt-2 text-lg">
-          کلید ارتباط موثر با جهان سخن گفتن به زبان جهانیان است. خدمات ترجمه تخصصی ما که در کلاس جهانی ارائه می‌شود به شما کمک می‌کند بازارهای جدیدی بیابید و خود را به خوبی به شرکای بین المللی معرفی کنید. همچنین، طیف گسترده مترجم‌های ما، شما را قادر می‌سازد تخصصی‌ترین متون آکادمیک را بر اساس استانداردهای پذیرش معتبرترین ژورنال‌ها ترجمه کنید.
-
-
+            کلید ارتباط موثر با جهان سخن گفتن به زبان جهانیان است. خدمات ترجمه
+            تخصصی ما که در کلاس جهانی ارائه می‌شود به شما کمک می‌کند بازارهای
+            جدیدی بیابید و خود را به خوبی به شرکای بین المللی معرفی کنید.
+            همچنین، طیف گسترده مترجم‌های ما، شما را قادر می‌سازد تخصصی‌ترین متون
+            آکادمیک را بر اساس استانداردهای پذیرش معتبرترین ژورنال‌ها ترجمه
+            کنید.
           </p>
-          <p className="text-start mt-2 text-md text-blue-900" dir="ltr">a@a.com</p>
+          <p className="text-start mt-2 text-md text-blue-900" dir="ltr">
+            a@a.com
+          </p>
         </div>
       </Modal>
       <AdminWarpper>
-        <TableMessage showModal={modalHandler}/>
+        <TableMessage showModal={modalHandler} />
       </AdminWarpper>
     </>
   );
